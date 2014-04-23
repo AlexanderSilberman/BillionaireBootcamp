58a51fec573c9c9bc31a9e01bd25623e50762f49
Vinay Joshi <vjoshi5@gmail.com>
2014-04-22 17:46:31 -0700

    added copywright symbol to bottom of page

--- billionairesite/contact/index.php
+++ billionairesite/contact/index.php
@@ -12,7 +12,23 @@
 </head>
 
 <body id = "contact">
-		
+<?php include_once("analyticstracking.php") ?>	
+	<script>
+	
+	function checkInput(){
+	var fname = document.getElementById('fname').value;
+	var lname = document.getElementById('lname').value;
+	var email = document.getElementById('email').value;
+	var subject = document.getElementById('subject').value;
+	var message = document.getElementById('message').value;
+	
+		if (fname == "" || lname == "" || email == "" || subject == "" || message == "") {
+			alert('Please fill out all the fields');
+		}
+	}
+	
+	</script>
+	
 
 <div id = "outercontainer">
 
@@ -20,11 +36,28 @@
 <br style = "clear:both;"/>
 <aside id = "sidebar">
 <div id="companyresearch"> 
+    <script>
+	function searchStock() {
+	
+	var stockTicker = document.getElementById('stockTicker').value;
+	
+	if (stockTicker != "") {
+		var url = "http://investing.money.msn.com/investments/stock-price?Symbol=" + stockTicker + "&ocid=qbeb";
+		var win = window.open(url, '_blank');
+		win.focus;
+	} else {
+		alert('Please enter a stock ticker symbol.')
+	}
+	
+	
+	}
+	
+    </script>
     
     	<p> <h2>Company Research </h2></p>
     <p id="sidebartext"> Search Any Company's Stock Quote!</p>
-    	<input type= "text" placeholder="e.g. AAPL" />
-    	<input type="submit" value="Research" class="submitbutton" />
+    	<input type= "text" id="stockTicker" placeholder="e.g. AAPL" />
+    	<input type="submit" value="Research" onclick="searchStock()" class="submitbutton" />
     
     	<br />
     	
@@ -52,106 +85,44 @@
     Please fill out the form below with your information or your message. 
     </p>
     <br />
-
-    <?php
     
-// define variables and set to empty values
-$fnameErr = $lnameErr = $emailErr = $subjectErr = $messageErr = "";
-$fname = $lname = $email = $subject = $message = "";
+    	<?php
+	if (!isset($_POST['Submit'])){
+	?>
 
-if ($_SERVER["REQUEST_METHOD"] == "POST")
-{
-   if (empty($_POST["fname"]))
-     {$fnameErr = "First name is required";}
-   else
-     {
-     $fname = test_input($_POST["fname"]);
-     // check if name only contains letters and whitespace
-     if (!preg_match("/^[a-zA-Z ]*$/",$fname))
-       {
-       $fnameErr = "Only letters and white space allowed"; 
-       }
-     }
-     
-     if (empty($_POST["lname"]))
-     {$lnameErr = "Last name is required";}
-   else
-     {
-     $lname = test_input($_POST["lname"]);
-     // check if name only contains letters and whitespace
-     if (!preg_match("/^[a-zA-Z ]*$/",$lname))
-       {
-       $lnameErr = "Only letters and white space allowed"; 
-       }
-     }
-   
-   if (empty($_POST["email"]))
-     {$emailErr = "Email is required";}
-   else
-     {
-     $email = test_input($_POST["email"]);
-     // check if e-mail address syntax is valid
-     if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email))
-       {
-       $emailErr = "Invalid email format"; 
-       }
-     }
-     
-   if (empty($_POST["subject"]))
-     {$subjectErr = "Subject is required";}
-   else
-     {
-     $subject = test_input($_POST["subject"]);
-     // check if name only contains letters and whitespace
-     if (!preg_match("/^[a-zA-Z ]*$/",$subject))
-       {
-       $subjectErr = "Only letters and white space allowed"; 
-       }
-     }
-     
-   if (empty($_POST["message"]))
-     {$messageErr = "Message is required";}
-   else
-     {
-     $message = test_input($_POST["message"]);
-     // check if name only contains letters and whitespace
-     if (!preg_match("/^[a-zA-Z ]*$/",$message))
-       {
-       $messageErr = "Only letters and white space allowed"; 
-       }
-     }
-}
-
-     function test_input($data)
-     {
-     $data = trim($data);
-     $data = stripslashes($data);
-     $data = htmlspecialchars($data);
-     return $data;
-}
-     
-    ?>
-
-
-	<form id="contactinfo" action="contact_info.php" method="post">
+	<form id="contactinfo" action="" method="post">
     <label> First Name </label>
-    	<input id="fname" type="text" placeholder= "John" name="fname" autofocus="autofocus" />
-	<span class="error">* <?php echo $fnameErr;?></span><br />
+    	<input id="fname" type="text" placeholder= "John" name="firstname" onblur="checkInput()" autofocus="autofocus" /> <br />
     <label> Last Name </label>
-    	<input id="lname" type="text" placeholder="Smith" name="lname"  />
-	<span class="error">* <?php echo $lnameErr;?></span><br />
+    	<input id="lname" type="text" placeholder="Smith" name="lastname"  /> <br />
     <label> Email Address </label>
-    	<input id="email"  type="email" placeholder="john.smith@gmail.com" name="email" />
-	<span class="error">* <?php echo $emailErr;?></span> <br />
+    	<input id="email" type="email" placeholder="john.smith@gmail.com" name="email" /> <br />
     <label> Message Subject </label>
-    	<input id="subject"  type="text" placeholder="Where can I find more about your books?" name="subject" size="40"/>
-	<span class="error">* <?php echo $subjectErr;?></span> <br />
+    	<input id="subject" type="text" placeholder="Where can I find more about your books?" name="subject" size="40"/> <br />
     <label> Your Message </label>
-    	<textarea placeholder="Message content" id="message"  name="message" style="width:300px; height:200px;"></textarea>
-	<span class="error">* <?php echo $messageErr;?></span> <br /> <br />
-
-    <input type="Submit" id="submitbutton" name="Submit" class = "submitbutton" value = "Submit" style="margin-left:400px;"/>
+    	<textarea placeholder="Message content" name="message" style="width:300px; height:200px;"></textarea> <br /> <br />
+    
+    <input type="Submit" name="Submit" class = "submitbutton" value = "Submit" style="margin-left:400px;"/>
     </form>
+<?php
+	} else {
+		if (isset($_POST['Submit'])){
+		
+		$to = "ganeshsundar93@gmail.com";
+		$fname = $_REQUEST['fname'];
+		$lname = $_REQUEST['lname'];
+		$email = $_REQUEST['email'];
+		$subject = $_REQUEST['subject'];
+		$message = $_REQUEST['message'];
+		mail($to, $subject, $message, $email);
+		
+		echo "Thanks for sending you feedback!";
+		}
+	}
+?>
+		
+	
+	
 		
 		
 
@@ -164,7 +135,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST")
 
     <div id="bottom">
     	<a href="http://www.nataliepace.com" id="footerlefttext">NataliePace.com</a>
-        <span id="footerrightttext"> &copy; Billionaire Bootcamp</span>
+        <span id="footerrightttext">© Billionaire Bootcamp</span>
     </div>
     
 </div>
