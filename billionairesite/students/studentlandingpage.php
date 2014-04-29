9f6e5d913b057adf7f4e37eb362f1d20bf7a1898
Vinay Joshi <vjoshi5@gmail.com>
2014-04-28 19:43:30 -0700

    added new index.php and renamed the glossasry site glossay.php

---
+++ billionairesite/students/studentlandingpage.php
@@ -0,0 +1,57 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Billionaire Bootcamp: Student Resources</title>
+<link rel="stylesheet" type="text/css" href="../stylesheet.css" />
+
+         <?php
+		 	include "../includes/navbar.php";
+		 ?>
+
+</head>
+
+<body id = "student">
+
+<div id = "outercontainer">
+	
+
+<br style = "clear:both;"/>
+
+<?php include "../includes/sidebar.php"; ?>
+
+
+
+<div id="main">
+
+	<p id="termstoknow">Student Resources</p>
+    
+    <p id="sub">Use the following three resources to help you become financially literate!</p>
+    
+    <p id="sub">
+        <ol>
+        	<li style="text-align:left"><a href="index.php">Glossary</a></li><br />
+        	<li style="text-align:left"><a href="investing.php">Top 11 Investing Mistakes</a></li><br />
+            <li style="text-align:left"><a href="cookupprofits.php">Recipe for Cooking Up Profits</a></li><br />
+        </ol>
+
+</div>
+
+</div>
+    <div style="clear:both" /> </div>
+
+<div id="footer">
+
+    <div id="bottom">
+    	<a href="http://www.nataliepace.com" id="footerlefttext">NataliePace.com</a>
+        <span id="footerrightttext">© Billionaire Bootcamp</span>
+    </div>
+    
+</div>
+
+
+
+
+</body>
+</html>
