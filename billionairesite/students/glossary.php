90b2939a701c5500640a99ac91fef7bc33d4ac18
Vinay Joshi <vjoshi5@gmail.com>
2014-04-28 19:48:34 -0700

    added glossary.php

---
+++ billionairesite/students/glossary.php
@@ -0,0 +1,140 @@
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Billionaire Bootcamp: Student Resources</title>
+<link rel="stylesheet" type="text/css" href="../stylesheet.css" />
+
+         <?php
+		 	include "../includes/navbar.php";
+		 ?>
+
+</head>
+
+<body id = "student">
+
+<div id = "outercontainer">
+	
+
+<br style = "clear:both;"/>
+
+<?php include "../includes/sidebar.php"; ?>
+
+
+
+<div id="main">
+
+	<p id="termstoknow"> 
+    Terms to Know
+    </p>
+    
+    <p id="sub"> 
+    Learn these tems to become a financial guru!
+    </p>
+    
+    <table id="terms"> 
+    <tr>
+    	<td class="terms"> ADR (American Depositary Receipt) </td>
+        <td class="definition">  This is how foreign companies trade on the U.S. stock exchanges. An ADR is a 	negotiable certificate issued by a U.S. bank representing a specific number of shares of a foreign company.  </td>
+    </tr>
+    
+    <tr>
+    	<td class="terms"> P/E (Price-to-Earnings Ratio)</td>
+        <td class="definition"> This is the price of a stock divided by its earnings per 
+share. It tells you how expensive the company’s stock is. A listing of “N/A” means that the 
+company is losing money. It’s not uncommon for a new company or a company that is going 
+through restructuring to lose money while it ramps up for new sales. General Motors lost money 
+in 2006 and 2007—major restructuring—and so did Wisdom Tree, a new exchange-traded funds 
+company with some of the top business leaders on Wall Street involved in the management and 
+advisement of the company. 
+ </td>
+    </tr>
+    
+    <tr>
+    	<td class="terms"> Earnings per share </td>
+        <td class="definition"> The earnings of the company are divided by the number of shares 
+outstanding. For instance, a corporation with $100 million in earnings and 100 million shares 
+outstanding would report earnings per share of $1. 
+ </td>
+    </tr>
+    
+    <tr>
+    	<td class="terms"> Sales and income </td>
+        <td class="definition">  Be sure to check both of these statistics. A company could have a lot 
+of sales, but still be losing money in income because the expenses are higher than the money the 
+corporation is bringing in. Additionally, rising trends in sales and earnings are positive signs, 
+whereas losing trends can be a sign of continued trouble ahead.</td>
+    </tr>
+    
+    <tr> 
+    	<td class="terms"> 52-week high and low </td>
+        <td class="definition"> Check the annual low and high prices to get a perspective on 
+whether or not you’re buying high on the year. Many sites also allow you to construct historical 
+charts of the share price with one easy click. You can look at the history of the company’s share 
+price for the past five to ten years and more very easily online. 
+ </td>
+    </tr>
+    
+    <tr> 
+    	<td class="terms"> Market capitalization and number of shares outstanding </td>
+        <td class="definition"> The market value of a company 
+is the product of the share price and the total number of common shares. Multibillion dollar corporations provide stability for your portfolio and are less likely to have huge price 
+fluctuations (either up or down). Companies with a smaller market capitalization, under one 
+billion, perform better on average than the rest of the marketplace, but they also have a higher 
+risk attached to that performance. 
+ </td>
+    </tr>
+    
+    <tr> 
+    	<td class="terms"> D/E (Debt/Equity Ratio) </td>
+        <td class="definition"> This is the total long-term debt compared to the market 
+capitalization of the company. Extremely high D/Es could indicate that the industry, sector or 
+company is not doing very well. If you see N/A in this column on the financial site’s stock page, 
+go to the most recent earnings report on the SEC’s web site (SEC.gov) to find out the long-term 
+debt, which is likely to be quite large. (Companies without any debt put 00.00 in the D/E 
+column.) 
+ </td>
+    </tr>
+    
+   <tr>
+   		<td class="terms"> Institutional Investors </td>
+        <td class="definition"> Institutional investors move the markets. If you’ve found a 
+company before the analysts discover it, you could benefit from increased institutional investors’ 
+interest. 
+ </td>
+   </tr>
+   
+  <tr>
+  	<td class="terms"> Operating Margins </td>
+    <td class="definition"> This is the difference between the cost of goods (net income) and the 
+price the company is able to charge for the product or service (net sales). </td>
+  </tr>
+    
+    
+    
+    </table>
+    
+    
+    
+      
+
+</div>
+
+</div>
+    <div style="clear:both" /> </div>
+
+<div id="footer">
+
+    <div id="bottom">
+    	<a href="http://www.nataliepace.com" id="footerlefttext">NataliePace.com</a>
+        <span id="footerrightttext">© Billionaire Bootcamp</span>
+    </div>
+    
+</div>
+
+
+
+
+</body>
+</html>
